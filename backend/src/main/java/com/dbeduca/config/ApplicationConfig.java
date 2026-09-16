607e671a3863fb089855f8081994c4941080afc9
Jucelio Coelho <juceliocoelho2022@gmail.com>
2026-09-16 16:02:15 -0300

    feat: configure Oracle JDBC execution

--- backend/src/main/java/com/dbeduca/config/ApplicationConfig.java
+++ backend/src/main/java/com/dbeduca/config/ApplicationConfig.java
@@ -1,13 +1,21 @@
 package com.dbeduca.config;
 
 import com.dbeduca.core.*;
+import com.dbeduca.execution.DatabaseExecutionAdapter;
+import com.dbeduca.execution.DatabaseExecutionService;
+import com.dbeduca.execution.OracleExecutionAdapter;
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
+
     @Bean
     ScriptGeneratorRegistry scriptGeneratorRegistry() {
         return new ScriptGeneratorRegistry(List.of(
@@ -16,5 +24,42 @@ public class ApplicationConfig {
             new MongoScriptGenerator(),
             new OracleScriptGenerator()
         ));
+    }
+
+    @Bean
+    DataSource oracleDataSource(
+        @Value("${dbeduca.oracle.jdbc-url}") String jdbcUrl,
+        @Value("${dbeduca.oracle.username}") String username,
+        @Value("${dbeduca.oracle.password}") String password
+    ) throws SQLException {
+
+        OracleDataSource dataSource =
+            new OracleDataSource();
+
+        dataSource.setURL(jdbcUrl);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    DatabaseExecutionAdapter oracleExecutionAdapter(
+        DataSource oracleDataSource
+    ) {
+        return new OracleExecutionAdapter(
+            oracleDataSource
+        );
+    }
+
+    @Bean
+    DatabaseExecutionService databaseExecutionService(
+        ScriptGeneratorRegistry scriptGeneratorRegistry,
+        List<DatabaseExecutionAdapter> adapters
+    ) {
+        return new DatabaseExecutionService(
+            scriptGeneratorRegistry,
+            adapters
+        );
     }
 }
